0624957d35ebc9f3743aecb2fd3b9c9e18cb864a
Poptamas Vlad <poptamasvlad@gmail.com>
2022-12-11 13:47:43 +0200

    Add project files.

---
+++ lab3ex3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace lab3ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Scrieti o functie care va determina daca un numar este sau nu patrat perfect. Apelati-o si afisati-i rezultatul */
+
+            int n = int.Parse(Console.ReadLine());
+
+            if(PatratPerfect(n))
+            {
+                Console.WriteLine(n + " este patrat perfect");
+            }
+            else
+            {
+                Console.WriteLine(n + " nu este patrat perfect");
+            }
+
+            static bool PatratPerfect(int n)
+            {
+                for(int i=0; i<=n; i++)
+                {
+                    if(i*i == n)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+}
